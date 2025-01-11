628c0a1ead8f31b634ec6ab887cfc7e627da4afa
Alison Simiao <alison.simiao@gmail.com>
2025-01-10 22:23:51 -0300

    add health

--- resource/user/user.route.go
+++ resource/user/user.route.go
@@ -3,6 +3,8 @@ package user
 import (
 	"time-wise/api"
 	"time-wise/middleware"
+
+	"github.com/gin-gonic/gin"
 )
 
 var uv = NewUserValidate()
@@ -11,6 +13,11 @@ func CreateRoutes() {
 	api := api.New()
 	uc := NewUserController()
 
+	api.CreateRoute("", "GET", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
 	api.CreateRoute("api/login", "POST", middleware.Validator(uv.Login), uc.Login)
 	api.CreateRoute("api/signup", "POST", middleware.Validator(uv.Create), uc.Create)
 }
